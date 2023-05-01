93b6e08ae61fa544b3db204adad06c133750e63b
Mika Hägerström <mikahagerstrom@hotmail.com>
2023-05-01 12:16:11 +0200

    cart slides in and out ease

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,27 +1,32 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import TopHeader from './components/Header/TopHeader.vue';
-import Footer from './components/Footer.vue';
+import ShopFooter from './components/ShopFooter.vue';
 import Cart from './components/Cart.vue';
 </script>
 
 <template>
   <div>
-    <div v-show="showCart">
-      <Cart/>
-    </div>
+    <!-- <div v-show="showCart"> -->
+      <Cart :showCart="showCart"/>
+    <!-- </div> -->
     <TopHeader @toggle-cart="toggleCart"/>
     <nav>
       <RouterLink to="/">Home</RouterLink>
     </nav>
       <RouterView />
-      <Footer />
+      <ShopFooter />
   </div>
 </template>
 
 <script>
 
 export default { 
+  components: {
+    TopHeader,
+    ShopFooter,
+    Cart,
+  },
   data() {
     return {
       showCart: false,
--- client/src/components/Cart.vue
+++ client/src/components/Cart.vue
@@ -1,45 +1,60 @@
 <template>
-  <Transition>
-    <div class="side-cart">
+  <Transition name="cart">
+    <div :class="[showCart ? 'show' : 'hide', 'side-cart']" >
       Hello, this is the cart
     </div>
-    <!-- <button v-b-toggle.sidebar-right>Toggle Sidebar</button>
-    <sidebar id="sidebar-right" title="Sidebar" right shadow>
-      <div class="px-3 py-2">
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        </p>
-        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
-      </div>
-    </sidebar> -->
 
 
   </Transition>
 </template>
 
 <script>
-// export default {
-//   props: {
-//     showCart: {
-//       type: Boolean,
-//       default: false,
-//     }
-//   },
-// }
+export default {
+  props: {
+    showCart: Boolean,
+  },
+}
 </script>
 
 <style lang="scss" scoped>
 .side-cart {
   position: fixed;
   top: 0;
+  // left: 0;
   right: 0;
   width: 300px;
   height: 100vh;
   background-color: #fff;
   box-shadow: 0 0 10px rgba(0,0,0,0.5);
   z-index: 999;
-  // transition: all 0.3s ease-in-out;
+  // transition: transform 0.5s ease-in-out;
   // transform: translateX(100%);
+
+  &.show {
+    transition: .5s ease;
+    // transform: translateX(100%);
+    
+  }
+
+  &.hide {
+    transition:  1s ease;
+    // transform: translateX(0);
+    right: -100%;
+    
+  }
 }
+
+
+
+// .cart-enter-active,
+// .cart-leave-active {
+//   transition: opacity 0.5s ease;
+//   transform: translateX(-100%);
+// }
+
+// .cart-enter-from,
+// .cart-leave-to {
+//   opacity: 0;
+//   transform: translateX(-100%);
+// }
 </style>
